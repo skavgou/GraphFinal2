48fff5096ecade553d3f9c0790e521a1679a2f15
unknown <kavans25@tcd.ie>
2024-12-29 13:11:40 +0000

    Updated display windows to make clear they can be closed

--- FinalProject/CharacterLoadingScreen.cpp
+++ FinalProject/CharacterLoadingScreen.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a026c10e5e1d2e74864f440134458761196f86676f4c1fe8eecde3ed26a0d040
-size 23734
+oid sha256:778ac40e99936ba320c90a4b321d2489ffcf1add06637ea77c44aee99498cb5d
+size 23757
